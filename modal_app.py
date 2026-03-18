2b22854c6a6987847dd28d74acf95cee87cf0e86
hholb <hholbrook@uchicago.edu>
2026-03-18 15:03:13 -0600

    remove template, just send down the manifests as json

--- modal_app.py
+++ modal_app.py
@@ -2,11 +2,7 @@ import modal
 import os
 
 app = modal.App("rootstock-admin")
-app.image = (
-    modal.Image.debian_slim()
-    .pip_install("fastapi[standard]", "jinja2")
-    .add_local_dir("./templates/", remote_path="/templates")
-)
+app.image = modal.Image.debian_slim().pip_install("fastapi[standard]")
 
 env = os.environ.get("MODAL_ENVIRONMENT", "dev")
 vol = modal.Volume.from_name(f"rootstock-admin-{env}", create_if_missing=True)
@@ -30,31 +26,15 @@ def manifest(manifest: dict):
 @modal.fastapi_endpoint()
 def dashboard():
     import json
-    from datetime import datetime
     from pathlib import Path
-    from fastapi.responses import HTMLResponse
-    from jinja2 import Environment, FileSystemLoader, select_autoescape
 
     vol.reload()
-
-    env = Environment(
-        loader=FileSystemLoader("/templates"),
-        autoescape=select_autoescape(),
-    )
-    t = env.get_template("index.html.jinja")
 
     manifest_path = Path("/data")
     manifest_files = manifest_path.glob("*.json")
     manifests = []
     for mf in manifest_files:
         with open(mf, "r") as f:
-            data = json.load(f)
-            if "last_updated" in data:
-                try:
-                    dt = datetime.fromisoformat(data["last_updated"])
-                    data["last_updated"] = dt.strftime("%b %d, %Y")
-                except ValueError:
-                    pass
-            manifests.append(data)
+            manifests.append(json.load(f))
 
-    return HTMLResponse(t.render(manifests=manifests))
+    return {"manifests": manifests}
